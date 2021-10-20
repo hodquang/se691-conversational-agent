3a22b0bc5708fb16634daca848998557f1474f01
qh74 <qh74@tux.cs.drexel.edu>
2021-10-20 19:23:40 -0400

    Andrew push

---
+++ Chatbot/App.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
+import { Dialogflow_V2 } from 'react-native-dialogflow';
+
+import { dialogflowConfig } from './env';
+
+const botAvatar = require('./assets/images/obama.jpeg');
+const BOT = {
+  _id: 2,
+  name: 'Mr.Bot',
+  avatar: botAvatar
+};
+
+class App extends Component{
+  state = {
+    messages: [
+      { _id: 2, text: 'My name is Obama', createdAt: new Date(), user: BOT },
+      { _id: 1, text: 'Hi', createdAt: new Date(), user: BOT }
+    ],
+    id: 1,
+    name: '',
+  }
+
+  componentDidMount() {
+    Dialogflow_V2.setConfiguration(
+      dialogflowConfig.client_email,
+      dialogflowConfig.private_key,
+      Dialogflow_V2.LANG_ENGLISH_US,
+      dialogflowConfig.project_id,
+    );
+  }
+
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+
+    let message = messages[0].text;
+
+    Dialogflow_V2.requestQuery(
+        message,
+        (result) => this.handleGoogleResponse(result),
+        (error) => console.log(error),
+    );
+  }
+
+  handleGoogleResponse(result) {
+    let text = result.queryResult.fulfillmentMessages[0].text.text[0];
+    console.log("result.= ", result)
+    
+    this.sendBotResponse(text);
+  }
+
+  sendBotResponse(text) {
+      let msg = {
+        _id: this.state.messages.length + 1,
+        text,
+        createdAt: new Date(),
+        user: BOT,
+      };
+
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, [msg]),
+    }));
+  }
+
+  render() {
+    return (
+        <View style={{flex: 1, backgroundColor: '#fff'}}>
+        <GiftedChat
+            messages={this.state.messages}
+            onSend={(message) => this.onSend(message)}
+            user={{ _id: 1 }}
+          />
+        </View>
+    );
+  }
+}
+
+export default App;
---
+++ Chatbot/env.js
@@ -0,0 +1,12 @@
+export const dialogflowConfig = {
+  "type": "service_account",
+  "project_id": "michaeljordan-qutj",
+  "private_key_id": "65a8f23be693c2b6cdf91b519aa6daea25a35c99",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDNDPHZkn3DoluZ\nsmKi8KcYvuZ0L3bcT225CY+q1f8Cp+/bsA1jgPunP45XC+JBNvW4u073eY7Yw7j+\n+vF/+S/2N8Pfl9BULBO47lO+enR5HgjPFoIlb0JFz4XDcgsrB2pUHOkLX7FNxWwn\nYI0FLX72ucsoe/31qUAwgDZsYmjXvsiNtu4wcswwDPv5HojLmwBe7WRAFe2RXmtY\nViaQwvaweYR1OiZM9PfUCh0lmFWDXZE3uVPNKIcMKKnxpmsJ7mjehA1/+kT/eWfU\n9U6EERp0KrxmvYBVF1MX/m3iO/DZTQdIuO2dY4luFY82N0uh3ib0vIk9K6ECsOX9\npbqwkT11AgMBAAECggEAC7LQXYRhDndF2Jf3zqoCLoQ9q6VFl74kWWk1PB7nZKRm\nJ9d8fA/Qy7LxkP/JkcTr8fUlxTK3NqC7/jgc7RsvZFG8KBaGOTcmjOdz+asM2Bgh\nkocLN82IL2lujT8FahgLNe9MUGzONcyspfZ5NTFqS/M/pvij/Y8BbYLzF+3pY12R\n+ABxGR9ykb/O59QuBu6ezbu57AUbBul6dadPYOdyiYfKmfXQLvlnIWzW1MMoHSiO\n08HX61fzWEeoG09kZedjjl+YF2boomFkMGi5UOezU1pCZtSt+WYx6R2UezL9vtW7\nzqACzIIixXfCr17BqOpVP/BacesCtNqT45xi51xDRQKBgQDpFqqXyrPSOkW/wvju\nYAS4CctJ2s9yFoyMvD/as8HCJHfFuYTY8NHZn0FowS8J4a+DwlgAIbltivTJeghs\nbGmFHr+nrQ2Ob7RKFf5po6WjEF6q/cQAsr9YA3RjBELABv/qc92jSfFfq7gKyu0w\n5KQM0eVdBNgPLdncDKBIgS+4VwKBgQDhNL5YnkNG1eo+6BaM2yUOLe77QqD+QGHm\nVmcnfg1cvSTcH2/dxb4Ngwtf3LWlKl0OdsWnZIKz4hWE7bCANDsM2IrI5Yw9bDvM\nwzciqOQ6LR65oOgwjH2vZwJMiuPVJ8jKGSGT0xLn2ifUCbFrfB1nJLdgpcZYzqiR\nJuLnNqCJEwKBgQCB9FQ6exZU4I5gwti9NqWK4lMII0nDhxTnplFHoy8lCeYJREzw\nExw85BC40fRi4wRTRrh/ok3a6D1gU3x8ztjqEmGyn8ZOIrN3FtoYldfUmVzeostz\nxDTwYkSyq/53R6/JTL5GuwQrXfcndUpXFzLrvt7NRNUdaWfsytFWe0ha0wKBgAGj\nnaMNe9XfJrZQgCGEUs+4m92uTXesRkoj5NpPu48qPUNw4yKNFuYIRyPqelP6a/T3\n/lZKj/z5L0jpZfh46NkeFlyB9zRXPlQuM4+IBSk7PNcDCc2YqC2HpWejNydNA9Qc\nvLbTuXaAivurGmR64cfnZ4JHkpsA3g84ZjxilGaLAoGAFdgE3vONGLj1gFQELwpR\nQTEAa2SsecXLUtpnerMUtvq0b/iafNgV8ggfGtJYwqls4id3qRvFggluH/N2qs2O\nXXcbvudeNT9kz6/tSZqdvlLGXSEqMCf0tPkvs2jRAIZuxVd5DvMZuygYw7bcy5sz\ncz4RlfCsghyxlKSAQHJhogQ=\n-----END PRIVATE KEY-----\n",
+  "client_email": "chatbot-lesson@michaeljordan-qutj.iam.gserviceaccount.com",
+  "client_id": "115170502893596623323",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/chatbot-lesson%40michaeljordan-qutj.iam.gserviceaccount.com"
+};
