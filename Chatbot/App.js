24c20cd610dfe3bca8b9cad3bbedbff079f50c13
Quang Ho <ho.d.quang@gmail.com>
2021-11-14 22:50:53 -0500

    a few updates

--- Chatbot/App.js
+++ Chatbot/App.js
@@ -5,12 +5,17 @@ import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './screens/Login';
 import SelectionScreen from './screens/SelectionScreen.js';
 import ChatScreen from './screens/ChatScreen.js';
+import { isIOS } from 'react-native-elements/dist/helpers';
+
+// npm install
+// cd ios
+// pod-install or npx pod-install
 
 const Stack = createStackNavigator();
 const MyStack = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Login" component={LoginScreen} />
+      {/* <Stack.Screen name="Login" component={LoginScreen} /> */}
       <Stack.Screen name="SelectionScreen" component={SelectionScreen} />
       <Stack.Screen name="ChatScreen" component={ChatScreen} />
     </Stack.Navigator>
--- Chatbot/screens/SelectionScreen.js
+++ Chatbot/screens/SelectionScreen.js
@@ -11,16 +11,19 @@ export default class SelectionScreen extends React.Component {
             <View style={styles.outsideContainer}>
                 <FlatList
                     data={[
-                        { id: 1, name: 'Barrack Obama', avatar: '/Users/dannydominguez/chatbot/assets/images/obama.jpeg', importName: 'obama', },
-                        { id: 2, name: 'Michael Jordan', avatar: '/Users/dannydominguez/chatbot/assets/images/jordan.jpeg', importName: 'kobe', },
-                        { id: 3, name: 'Michael Jackson', avatar: '/Users/dannydominguez/chatbot/assets/images/jackson.jpeg' },
-                        { id: 4, name: 'Martin Luther King', avatar: '/Users/dannydominguez/chatbot/assets/images/MLK.jpeg' },
-                        { id: 5, name: 'Abraham Lincoln', avatar: '/Users/dannydominguez/chatbot/assets/images/lincoln.jpeg' },
+                        { id: 1, name: 'Barrack Obama', avatar: '/Users/quangho/se691/Chatbot/assets/images/obama.jpeg', importName: 'obama', },
+                        { id: 2, name: 'Michael Jordan', avatar: '/Users/quangho/se691/Chatbot/assets/images/jordan.jpeg', importName: 'kobe', },
+                        { id: 3, name: 'Michael Jackson', avatar: '/Users/quangho/se691/Chatbot/assets/images/jackson.jpeg' },
+                        { id: 4, name: 'Martin Luther King', avatar: '/Users/quangho/se691/Chatbot/assets/images/MLK.jpeg' },
+                        { id: 5, name: 'Abraham Lincoln', avatar: '/Users/quangho/se691/Chatbot/assets/images/lincoln.jpeg' },
                     ]}
                     keyExtractor={(item) => item.id}
                     renderItem={({ item }) => (
                         <TouchableOpacity onPress={() =>
-                            this.props.navigation.navigate('ChatScreen', { botName: item.name, botId: item.id, avatar: item.avatar, name: this.props.route.params.name, id: this.props.route.params.id })}>
+                            this.props.navigation.navigate('ChatScreen', {
+                                botName: item.name, botId: item.id, avatar: item.avatar,
+                                // name: this.props.route.params.name, id: this.props.route.params.id,
+                            })}>
                             <View style={styles.profile}>
                                 <View style={styles.leftContainer}>
                                     <Image source={{ uri: item.avatar }} style={styles.avatar} />
