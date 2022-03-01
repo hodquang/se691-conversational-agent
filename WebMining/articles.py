dafe8e47c9cd46a13c2cd8b3b6ea1af5c0e0a697
Shaquille <shaquille@Shaquilles-MacBook-Pro.local>
2022-02-28 19:11:33 -0500

    Add support for Michael Jackson

--- WebMining/articles.py
+++ WebMining/articles.py
@@ -1,6 +1,10 @@
 # Data URLs to be use for web scraping and Agent SOTs
 
+# Comment the articles you don't want to process.
+
 # Expand this to support non-wikipedia urls
 URLS_TO_PARSE = [
-	"https://en.wikipedia.org/wiki/Barack_Obama",
+#	"https://en.wikipedia.org/wiki/Barack_Obama",
+#	"https://en.wikipedia.org/wiki/Michael_Jordan",
+	"https://en.wikipedia.org/wiki/Michael_Jackson",
 ]
